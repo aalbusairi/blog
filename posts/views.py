49db73c84c0ae09055cdae75ee5f5579abed80c5
aalbusairi <aalbusairi.code@gmail.com>
2017-07-25 18:01:10 +0300

    Update/edit view completed

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
 from .models import Post
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
 
 def home(request):
 	return render(request, 'home.html', {})
 
 def post_create(request):
-	return render(request, 'post_create.html', {})	
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect("posts:list")
+	context = {
+	"form": form
+	}
+	return render(request, 'post_create.html', context)	
 
-def post_update(request):
-	return render(request,	'post_update.html', {})	
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		return redirect("posts:list")
+	context = {
+	"form":form,
+	"post_object":post_object,
+	}
+	return render(request,	'post_update.html', context)	
 
 def post_delete(request):
 	return render(request,	'post_delete.html', {})		
