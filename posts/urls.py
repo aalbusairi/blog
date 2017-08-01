fb99be2ab214b936ea0a06b69e83425c7cab59f8
aalbusairi <aalbusairi.code@gmail.com>
2017-08-01 19:16:44 +0300

    Signup, Login, Logout

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 	url(r'^delete/(?P<slug>[-\w]+)/$', views.post_delete, name="delete"),
 	url(r'^list/$', views.post_list, name="list"),
 	url(r'^detail/(?P<slug>[-\w]+)/$', views.post_detail, name="detail"),
+	url(r'^signup/$', views.usersignup, name="signup"),
+	url(r'^login/$', views.userlogin, name="login"),
+	url(r'^logout/$', views.userlogout, name="logout"),
 ]
