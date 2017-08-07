7d34faee3947f22f7a04d29ee35cb32e920a63f7
aalbusairi <aalbusairi.code@gmail.com>
2017-08-07 20:42:18 +0300

    api views

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from posts.models import Post
+
+class PostListSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ['title', 'author', 'slug', 'content','publish',]
+
+class PostDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ['title', 'author', 'slug', 'content','publish', 'draft', 'id',]
+
+class PostCreateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ['title', 'content','publish','draft']		
